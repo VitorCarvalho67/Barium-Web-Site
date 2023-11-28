4cc1eeccc9a3faf9ea570fa4612bdfe6d5af901c
VitorCarvalho67 <vitor.carvalho67@etec.sp.gov.br>
2023-11-28 09:07:06 -0300

    Add script for btns section

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function GoToHome (){
     window.location.assign("/");
 }
+
+function Ativar(num){
+    num = num.toString();
+
+    const ativoAtual = document.querySelector("#ativo");
+    ativoAtual.id = "";
+
+    const ativo = document.querySelector(`.a${num}`);
+    ativo.id = "ativo";
+
+    const conteudoAtual = document.querySelector("#conteudo");
+    conteudoAtual.id = "";
+
+    const conteudo = document.querySelector(`.conteudo${num}`);
+    conteudo.id = "conteudo";
+}
